f0169ab2e71d21429c89134208b8ba5098f4f8f5
Tom Dicks <tom@ubermensch.tv>
2016-11-09 21:13:01 +0000

    Added hacky short simulation to GPIODummy

--- PogoTestApp/RPiDummy/GPIODummy.py
+++ PogoTestApp/RPiDummy/GPIODummy.py
@@ -1,6 +1,7 @@
 "Dummy module for using RPi.GPIO methods on Windows."
 
 _pins = {}
+_links = []
 
 # RPi.GPIO flags
 BCM = 11
@@ -14,7 +15,10 @@ LOW = 0
 for pin in range(1, 40):
     _pins[pin] = {}
     _pins[pin]["level"] = LOW
-    _pins[pin]["mode"] = OUT
+    _pins[pin]["mode"] = -1
+
+def _short(pin1, pin2):
+    _links.append((pin1, pin2))
 
 def setmode(mode):
     pass
@@ -30,8 +34,6 @@ def setup(pin, mode):
     else:
         set(pin)
 
-
-
 def cleanup():
     _pins = {}
 
@@ -39,4 +41,21 @@ def output(pin, level):
     _pins[pin]["level"] = level
 
 def input(pin):
-    return _pins[pin]["level"]
+    # If the pins is an input pin, check to see if it's "shorted" to an output pin
+    if _pins[pin]["mode"] == IN:
+
+        # Loop through the links. If any linked pin is an output pin, return it's output
+        for k, link in enumerate(_links):
+            pin1 = link[0]
+            pin2 = link[1]
+            if pin1 == pin and _pins[pin2]["mode"] == OUT:
+                return _pins[pin2]["level"]
+            if pin2 == pin and _pins[pin1]["mode"] == OUT:
+                return _pins[pin1]["level"]
+
+        # If there's no shorts, it's LOW.
+        return LOW
+
+    # Otherwise if the pin is an output pin, return what it's doing.
+    elif _pins[pin]["mode"] == OUT:
+        return _pins[pin]["level"]
