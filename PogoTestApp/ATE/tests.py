57baa69f020dafd42c82866aa3d2fc05e4eb2a6a
Daniel Cameron <55590059+Captec-Ltd-DC@users.noreply.github.com>
2019-11-06 10:44:39 +0000

    Update tests.py

--- PogoTestApp/ATE/tests.py
+++ PogoTestApp/ATE/tests.py
@@ -381,9 +381,9 @@ class TestPWR_4(TestProcedure):
             # Stage 2
             self.suite.form.append_text_line("Testing stage 2")
 
-            if (ad5.voltage_near(3.5, 0.1) and
-                ad6.voltage_between(0.8, 0.2) and
-                ad8.voltage_near(4.9, 0.2)):
+            if (ad5.voltage_near(3.95, 0.1) and
+                ad6.voltage_between(0.0, 0.2) and
+                ad8.voltage_near(4.9, 0.3)):
 
                 digio.set_high(DOP12_BAT1_GPIO)
                 digio.set_low(DOP13_BAT0_GPIO)
