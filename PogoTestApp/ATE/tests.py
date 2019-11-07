8a27d167b9308cecae017164a301286f1056d70d
Daniel Cameron <55590059+Captec-Ltd-DC@users.noreply.github.com>
2019-11-07 14:18:22 +0000

    Update tests.py

--- PogoTestApp/ATE/tests.py
+++ PogoTestApp/ATE/tests.py
@@ -289,25 +289,35 @@ class TestPWR_3(TestProcedure):
         # Step 1
         self.suite.form.append_text_line("Testing stage 1")
 
-        if (ad5.voltage_near(2.6, 0.2) and 
-            ad6.voltage_near(1.5, 0.2) and 
-            ad7.voltage_between(0, 2.60) and 
-            ad8.voltage_between(0, 1.50)):
+        #if (ad5.voltage_near(2.6, 0.2) and 
+            #ad6.voltage_near(1.5, 0.2) and 
+            #ad7.voltage_between(0, 2.60) and 
+            #ad8.voltage_between(0, 1.50)):
+        if (ad5.voltage_near(3.10, 0.2) and 
+            ad6.voltage_near(1.82, 0.2) and 
+            ad7.voltage_near(5.0, 0.15) and 
+            ad8.voltage_near(5.0, 0.15)):
             
             digio.set_high(DOP6_T_SW_ON)
-
+            #added
+            digio.set_high(DOP13_BAT0_GPIO)
             self.wait(0.1)
             
           # Step 2
             self.suite.form.append_text_line("Testing stage 2")
 
-            if (ad5.voltage_near(3.10, 0.2) and 
-                ad6.voltage_near(1.82, 0.2) and 
-                ad7.voltage_near(5.0, 0.15) and 
-                ad8.voltage_near(5.0, 0.15)):
+            #if (ad5.voltage_near(3.10, 0.2) and 
+                #ad6.voltage_near(1.82, 0.2) and 
+                #ad7.voltage_near(5.0, 0.15) and 
+                #ad8.voltage_near(5.0, 0.15)):
+            if (ad5.voltage_near(2.6, 0.2) and 
+                ad6.voltage_near(1.5, 0.2) and 
+                ad7.voltage_between(0, 2.60) and 
+                ad8.voltage_between(0, 1.50)):
             
-                digio.set_high(DOP13_BAT0_GPIO)
-
+                #digio.set_high(DOP13_BAT0_GPIO)
+                #added
+                digio.set_low(DOP13_BAT0_GPIO)
                 self.wait(0.5)
 
                 # Step 3
